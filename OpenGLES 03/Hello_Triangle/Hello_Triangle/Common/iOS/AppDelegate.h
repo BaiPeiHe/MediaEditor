d49d62cf8bea8a78109cc71e75f8a1f56847b892
白鹤 <392275709@qq.com>
2016-12-29 17:30:56 +0800

    中 - GLES03 绘制三角形

---
+++ OpenGLES 03/Hello_Triangle/Hello_Triangle/Common/iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello_Triangle
+//
+//  Created by 白鹤 on 2016/12/29.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES 03/Hello_Triangle/Hello_Triangle/Common/iOS/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello_Triangle
+//
+//  Created by 白鹤 on 2016/12/29.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface RootViewController : GLKViewController
+
+
+@end
+
